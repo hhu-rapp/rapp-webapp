713f8809f5b9658f97c04ff41b3faff9d7119079
Karsten Packeiser <karsten.packeiser@hhu.de>
2022-10-10 16:02:10 +0200

    add register feature

--- RAPP/auth.py
+++ RAPP/auth.py
@@ -1,19 +1,46 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for
 from flask import render_template
 
-from RAPP.forms import LoginForm
+from .import db
+from .forms import LoginForm, RegisterForm
+from .models import User
 
 bp = Blueprint('auth', __name__)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
-    email: str = None
+    email: str | None = None
     form: LoginForm = LoginForm()
 
     if form.validate_on_submit():
         email = form.email.data
 
-    return render_template('auth/login.html',
-                           email=email,
-                           form=form)
+    return render_template(
+        'auth/login.html',
+        email=email,
+        form=form
+    )
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    email: str | None = None
+    form: RegisterForm = RegisterForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+
+        user: User = User(email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('auth.login'))
+
+    return render_template(
+        'auth/register.html',
+        email=email,
+        form=form
+    )
--- RAPP/models.py
+++ RAPP/models.py
@@ -1,2 +1,26 @@
 """Data models."""
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from . import db
+from auth import login
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(128), nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<User {self.username}>'
+
+    def set_password(self, new_password) -> None:
+        self.password = generate_password_hash(new_password)
+
+    def check_password(self, password) -> bool:
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
