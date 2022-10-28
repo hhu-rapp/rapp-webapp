736019df03423950eb794b98f718d09929cfce95
Karsten Packeiser <karsten.packeiser@hhu.de>
2022-10-28 13:00:27 +0200

    add query form

--- RAPP/forms.py
+++ RAPP/forms.py
@@ -6,10 +6,12 @@ from flask_wtf.file import FileRequired
 from flask_wtf import FlaskForm
 from wtforms import EmailField
 from wtforms import PasswordField
+from wtforms import SelectField
 from wtforms import SubmitField
 from wtforms.validators import DataRequired
 from wtforms.validators import Email
 from wtforms.validators import EqualTo
+from wtforms.validators import InputRequired
 from wtforms.validators import Length
 from wtforms.validators import ValidationError
 
@@ -22,15 +24,13 @@ class DatabaseUploadForm(FlaskForm):
     ALLOWED_EXTENSIONS: list[str] = ['sqlite', 'sqlite3', 'db', 'db3', 's3db',
                                      'sl3']
 
-    file_db: FileField = FileField(
-        'DatabaseFile',
+    ml_db_file: FileField = FileField(
+        'New database',
         validators=[FileRequired(), FileAllowed(ALLOWED_EXTENSIONS)]
     )
 
-    submit = SubmitField('Log In')
-
-    def validate_file_db(self, file):
-        """Validate file_db input regarding sqlite format."""
+    def validate_ml_db_file(self, file):
+        """Validate ml_db_file input regarding sqlite format."""
 
         # TODO
         pass
@@ -51,6 +51,26 @@ class LoginForm(FlaskForm):
     )
 
     submit = SubmitField('Log In')
+
+
+class QuerySelectForm(FlaskForm):
+    """Query Selection form"""
+    query = SelectField(
+        'Select Query',
+        validators=[InputRequired()]
+    )
+    submit = SubmitField('Select')
+
+    def edit_queries(self, choices: list[tuple[str, str]]) -> None:
+        """Set query choices.
+
+        Parameters
+        ----------
+        choices: list[tuple[str, str]]
+            The queries the Query-SelectField will offer to the user
+            as a list of (value, label) pairs.
+        """
+        self.query.choices = choices
 
 
 class RegisterForm(FlaskForm):
