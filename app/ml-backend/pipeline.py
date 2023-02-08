28fad358b3a8a7bbe3272bcbe0352381244411d7
Manh Khoi Duong <duong@hhu.de>
2023-02-08 15:59:52 +0100

    Update pipeline.py

---
+++ app/ml-backend/pipeline.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+def preprocess_data(X):
+    """
+    Preprocesses X data.
+
+    Parameters
+    ----------
+    X : Dataframe
+
+    Returns
+    -------
+    X : Dataframe
+        Preprocessed X data.
+    """
+    categorical = X.select_dtypes(exclude=["number"]).columns
+    
+    # Adapt to categorical data.
+    if len(categorical) != 0:
+        categorical = pd.get_dummies(data=X[categorical], columns=categorical)
+        X = pd.concat([X, categorical], axis=1)
+        # Remove old categorical attributes from input features
+        X = X.drop(categorical, axis=1)
+
+    return X
+
+def predict(model, X, label):
+    """
+    Predicts on X data.
+
+    Parameters
+    ----------
+    model : scikit estimator
+        Model to be used for prediction.
+    X : Dataframe
+        Data to be predicted on.
+    label : str
+        Target variable to be predicted on in X.
+    """
+    if label in X.columns:
+        y = X.pop(label, axis=1)
+    else:
+        y = None
+        
+    X = preprocess_data(X)
+    
+    # 1 is the worst case outcome for the target variable
+    X['Prediction'] = model.predict_proba(X)[:, 1]
+    
+    return X
+    
+    
+def threshold(proba, value=0.80):
+    """
+    Returns threshold for prediction.
+    
+    Parameters
+    ----------
+    proba : (n_samples, 1)
+        Probability to be used as threshold.
+    value : float
+        Value to be used as threshold.
+    """
+    return proba >= value
