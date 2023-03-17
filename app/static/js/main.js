1119e290c264cde895281bf213b1b59295357e7c
Jose <jose.cordova@uni-duesseldorf.de>
2023-03-17 10:15:25 +0100

    Remove materialize js

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,21 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems);
-});
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
-  });
 
 $(document).ready(function () {
     $('#query-results').DataTable();
     $('#T_prediction').DataTable({
        "scrollX": true});
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, {});
 });
 
-$(document).ready(function () {
-    $('.sidenav').sidenav();
 });
