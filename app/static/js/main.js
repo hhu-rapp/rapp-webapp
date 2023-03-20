653bb33758b47c1879b563b54ab25d194e03f414
Jose <jose.cordova@uni-duesseldorf.de>
2023-03-20 17:18:07 +0100

    Add confirm modal component

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -4,6 +4,16 @@ $(document).ready(function () {
     $('#query-results').DataTable();
     $('#T_prediction').DataTable({
        "scrollX": true});
+    var modals = document.querySelectorAll('.modal');
+    var modalOptions = {
+        backdrop: true,
+        keyboard: true,
+        focus: true
+    };
+    
+ modals.forEach(function(modal) {
+  var myModal = new bootstrap.Modal(modal, modalOptions);
+});
 });
 
 document.addEventListener("DOMContentLoaded", function () {
