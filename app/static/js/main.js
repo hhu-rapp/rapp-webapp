48594d20f74e74a94bbdca2b732b725c15dadd91
Jose <jose.cordova@uni-duesseldorf.de>
2023-04-20 18:18:03 +0200

    Implement group level predictions

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -34,3 +34,61 @@ $('#prediction-tabs a[data-toggle="tab"]').on('show.bs.tab', function (e) {
     $(target)
         .addClass('active show')
 });
+
+
+// Group-level Prediction
+$(function () {
+    $('.feature-link').click(function (event) {
+        event.preventDefault(); // prevent the link from following the href
+
+        const selectedFeature = $(this).text();
+
+        // Update the feature button text
+        var featureSelect = document.getElementById('featureSelectButton');
+        featureSelect.innerHTML = selectedFeature;
+        // Get the current aggregation
+        var aggSelect = document.getElementById('aggregationSelectButton');
+        var currentAgg = aggSelect.textContent;
+
+        // Get the data
+        $.getJSON(/group_level_prediction/ + $SCRIPT_PARAMS, {
+            feature: selectedFeature,
+            aggregation: currentAgg,
+        }, function (data) {
+            $('#T_group_prediction').DataTable().destroy();
+            $('#group-pred-table').html(data.styled_df);
+            $('#T_group_prediction').DataTable({
+                "scrollX": true
+            });
+
+        });
+
+        return false;
+    });
+
+    $('.aggregation-link').click(function (event) {
+        event.preventDefault(); // prevent the link from following the href
+
+        const selectedAgg = $(this).text();
+        // Update the aggregation button text
+        var aggSelect = document.getElementById('aggregationSelectButton');
+        aggSelect.innerHTML = selectedAgg;
+        // Get the current feature
+        var featureSelect = document.getElementById('featureSelectButton');
+        var currentFeature = featureSelect.textContent;
+        // Get the data
+        $.getJSON(/group_level_prediction/ + $SCRIPT_PARAMS, {
+            feature: currentFeature,
+            aggregation: selectedAgg,
+        }, function (data) {
+            $('#T_group_prediction').DataTable().destroy();
+            $('#group-pred-table').html(data.styled_df);
+            $('#T_group_prediction').DataTable({
+                "scrollX": true
+            });
+
+        });
+
+        return false;
+    });
+});
