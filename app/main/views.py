f4cea030637dd111dab35a4a92231fd18f0ab277
Jose <jose.cordova@uni-duesseldorf.de>
2023-06-23 15:19:56 +0200

    Implement ajax calls to dashboard

--- app/main/views.py
+++ app/main/views.py
@@ -25,8 +25,9 @@ def index():
     page_title = "Dashboard"
     news: News = News.query.order_by(News.timestamp.desc()).all()
 
-    notifications = [{"link": '/prediction/1/1/1', "title": 'New Prediction', "mins_ago": 10}, {"link": '/performance_history', "title": 'New Performance History', "mins_ago": 15}]
-    majors = ['Informatik', 'Sozialwissenschaften', 'Wirtschaftswissenschaften', 'Rechtswissenschaften']
+    notifications = [{"link": '/prediction/1/1/1', "title": 'New Prediction', "mins_ago": 10},
+                     {"link": '/performance_history', "title": 'New Performance History', "mins_ago": 15}]
+    majors = ['Informatik', 'Sozialwissenschaften', 'Wirtschaftswissenschaften', 'Rechtswissenschaften', 'all']
     return render_template('main/dashboard.html', news=news, notifications=notifications, page_title=page_title,
                            majors=majors)
 
@@ -40,8 +41,8 @@ def dashboard_data(major_id, degree_id):
     # generate dummy risk analysis data
     dropout_df = generate_risk_analysis(200)
 
-    avg_bachelor_semesters = None
-    avg_master_semesters = None
+    avg_bachelor_semesters = '-'
+    avg_master_semesters = '-'
 
     if major_id == 'all':
         performance_df = performance_df
@@ -50,11 +51,11 @@ def dashboard_data(major_id, degree_id):
         performance_df = performance_df[performance_df['Major'] == major_id]
         dropout_df = dropout_df[dropout_df['Major'] == major_id]
 
-    if degree_id == 'Bachelor':
+    if degree_id == 'bachelor':
         performance_df = performance_df[performance_df['Degree'] == 'Bachelor']
         dropout_df = dropout_df[dropout_df['Degree'] == 'Bachelor']
 
-    elif degree_id == 'Master':
+    elif degree_id == 'master':
         performance_df = performance_df[performance_df['Degree'] == 'Master']
         dropout_df = dropout_df[dropout_df['Degree'] == 'Master']
 
@@ -68,14 +69,14 @@ def dashboard_data(major_id, degree_id):
     group_performance = performance_df.groupby('Matrikel_Nummer').agg(
         {'ECTS': 'sum', 'Num_Semester': 'max', 'Degree': 'max'}).reset_index()
 
-    if degree_id != 'Master':
+    if degree_id != 'master':
         # get students that have 180 ECTS and are in bachelor
         bachelor_students = group_performance[
             (group_performance['ECTS'] >= 180) & (group_performance['Degree'] == 'Bachelor')]
         # get avg number of semesters for bachelor students
         avg_bachelor_semesters = round(bachelor_students['Num_Semester'].mean(), 0)
 
-    if degree_id != 'Bachelor':
+    if degree_id != 'bachelor':
         # get students that have 120 ECTS and are in master
         master_students = group_performance[
             (group_performance['ECTS'] >= 120) & (group_performance['Degree'] == 'Master')]
@@ -83,18 +84,20 @@ def dashboard_data(major_id, degree_id):
         avg_master_semesters = round(master_students['Num_Semester'].mean(), 0)
 
     # avg ects per semester
-    group_performance = performance_df.groupby(['Semester', 'Year']).agg(
+    group_performance = performance_df.groupby(['Matrikel_Nummer', 'Semester', 'Year']).agg(
         {'ECTS': 'sum'}).reset_index()
     avg_ects_per_semester = round(group_performance['ECTS'].mean(), 0)
 
     # get data for plots get only unique Matrikel_Nummer
-    group_performance = performance_df.groupby('Matrikel_Nummer').agg({'Sex': 'max', 'Nationality': 'max'}).reset_index()
-    sex_plot = group_performance['Sex'].value_counts().reset_index().sort_values(by='Sex', ascending=False).to_dict(
-        'list')
+    group_performance = performance_df.groupby('Matrikel_Nummer').agg(
+        {'Sex': 'max', 'Nationality': 'max'}).reset_index()
+
+    sex_plot = group_performance['Sex'].value_counts().reset_index().sort_values(by='Sex', ascending=False).rename(
+        columns={'index': 'labels', 'Sex': 'data'})
 
     nat_plot = group_performance['Nationality'].value_counts().reset_index().sort_values(by='Nationality',
-                                                                                         ascending=False).to_dict(
-        'list')
+                                                                                         ascending=False).rename(
+        columns={'index': 'labels', 'Nationality': 'data'})
 
     # add all values to response
     response = {'totalStudents': total_students,
@@ -102,8 +105,8 @@ def dashboard_data(major_id, degree_id):
                 'avgBachelorSemesters': avg_bachelor_semesters,
                 'avgMasterSemesters': avg_master_semesters,
                 'avgEctsPerSemester': avg_ects_per_semester,
-                'sexPlot': sex_plot,
-                'natPlot': nat_plot}
+                'sexPlot': json.dumps(sex_plot.to_dict('list')),
+                'natPlot': json.dumps(nat_plot.to_dict('list'))}
 
     response = json.dumps(response)
 
