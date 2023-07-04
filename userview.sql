53f7693fe3ac29a2bfb5e2109ee38133576b665c
Manh Khoi Duong <duong@hhu.de>
2023-07-04 14:34:25 +0200

    Add Fachsemester selection

--- userview.sql
+++ userview.sql
@@ -9,5 +9,6 @@ WHERE S.Pseudonym = 2754842
 AND S.Pseudonym = SSP.Pseudonym
 AND SSP.Version = P.Version
 AND SSP.Nummer = P.Nummer
+AND SSP.Fachsemester = 1 
 GROUP BY S.Pseudonym, SSP.Studienfach, SSP.Version, SSP.Nummer, P.Modul, SSP.ECTS, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester
 ORDER BY SSP.Fachsemester
