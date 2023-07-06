17cf621c1f6b6ef4bdf9ef5b26e1e64a94ba602c
Jose <jose.cordova@uni-duesseldorf.de>
2023-07-06 21:17:51 +0200

    Add exams performance query to db

--- dev_setup.py
+++ dev_setup.py
@@ -28,11 +28,35 @@ def setup_queries_and_models(uploads: str, db_id: int) -> None:
             model = Model(name=model_name, filename=os.path.relpath(model_path, uploads), query_id=query.id)
             model.save()
 
+    # Add exams performance query
+    sql_string = """
+                SELECT S.Pseudonym, SSP.Studienfach, SSP.Version, SSP.Nummer, P.Modul, SSP.ECTS, SSP.Note, SSP.Status, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester, AVG(SSP2.Note) AS Durchschnittsnote
+                FROM Student AS S, Student_schreibt_Pruefung AS SSP, Pruefung AS P
+                JOIN (
+                  SELECT *
+                  FROM Student_schreibt_Pruefung
+                  WHERE Note IS NOT NULL
+                ) AS SSP2 ON SSP.Version = SSP2.Version AND SSP.Nummer = SSP2.Nummer AND SSP.Semesterjahr = SSP2.Semesterjahr AND SSP.Sommersemester = SSP2.Sommersemester
+                WHERE S.Pseudonym = var_pseudonym
+                AND S.Pseudonym = SSP.Pseudonym
+                AND SSP.Version = P.Version
+                AND SSP.Nummer = P.Nummer
+                AND SSP.Fachsemester = var_faculty_semester
+                GROUP BY S.Pseudonym, SSP.Studienfach, SSP.Version, SSP.Nummer, P.Modul, SSP.ECTS, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester
+                ORDER BY SSP.Fachsemester
+                """
+    
+    query = Query(name='exams_performance', query_string=sql_string)
+    query.save()
+
+    print(f'exams_performance query added at {query.id}')
 
 if __name__ == '__main__':
     app = create_app('development')
 
     with app.app_context():
+        # Delete all tables
+        db.drop_all()
         # Create the database
         db.create_all()
 
