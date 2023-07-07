b43f518bcfb4ab701194a0e7b8687a8911b543bb
Jose <jose.cordova@uni-duesseldorf.de>
2023-07-07 14:07:01 +0200

    Remove unnecessary columns from exam query

--- dev_setup.py
+++ dev_setup.py
@@ -30,22 +30,22 @@ def setup_queries_and_models(uploads: str, db_id: int) -> None:
 
     # Add exams performance query
     sql_string = """
-                SELECT S.Pseudonym, SSP.Studienfach, SSP.Version, SSP.Nummer, P.Modul, SSP.ECTS, SSP.Note, SSP.Status, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester, AVG(SSP2.Note) AS Durchschnittsnote
+                SELECT P.Modul, SSP.ECTS, SSP.Status, SSP.Note, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester, AVG(SSP2.Note) AS Durchschnittsnote
                 FROM Student AS S, Student_schreibt_Pruefung AS SSP, Pruefung AS P
                 JOIN (
                   SELECT *
                   FROM Student_schreibt_Pruefung
                   WHERE Note IS NOT NULL
                 ) AS SSP2 ON SSP.Version = SSP2.Version AND SSP.Nummer = SSP2.Nummer AND SSP.Semesterjahr = SSP2.Semesterjahr AND SSP.Sommersemester = SSP2.Sommersemester
-                WHERE S.Pseudonym = var_pseudonym
+                WHERE S.Pseudonym = %pseudonym%
                 AND S.Pseudonym = SSP.Pseudonym
                 AND SSP.Version = P.Version
                 AND SSP.Nummer = P.Nummer
-                AND SSP.Fachsemester = var_faculty_semester
+                AND SSP.Fachsemester = %semester%
                 GROUP BY S.Pseudonym, SSP.Studienfach, SSP.Version, SSP.Nummer, P.Modul, SSP.ECTS, SSP.Versuch, SSP.Fachsemester, SSP.Hochschulsemester
                 ORDER BY SSP.Fachsemester
                 """
-    
+
     query = Query(name='exams_performance', query_string=sql_string)
     query.save()
 
