4b79d6eab42a552153e8c8faf79feaf75a637c04
Jose <jose.cordova@uni-duesseldorf.de>
2023-08-18 16:54:46 +0200

    Optimize for executables

--- rapp.py
+++ rapp.py
@@ -1,10 +1,18 @@
 import os
+import sys
+
 from flask import Flask
 
 from app import create_app, db
 from app.models import Model, User
 
-app: Flask = create_app(os.getenv('FLASK_CONFIG') or 'default')
+# Change working directory to the directory of the executable
+if getattr(sys, 'frozen', False):
+    os.chdir(sys._MEIPASS)
+    app: Flask = create_app('development')
+    app.run(debug=True)
+else:
+    app: Flask = create_app(os.getenv('FLASK_CONFIG') or 'default')
 
 
 @app.shell_context_processor
