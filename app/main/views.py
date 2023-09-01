abaa5dde4bde34f1b6558dff0ca01a32d18930e2
Jose <jose.cordova@uni-duesseldorf.de>
2023-09-01 09:55:11 +0200

    Use functional queries instead of generated data

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,13 @@
 import numpy as np
 import json
 
-from flask import render_template, jsonify
+from flask import render_template, jsonify, abort
 from flask_login import current_user, login_required
 
 from . import main
+from ..ml_backend import query_database
 from ..ml_backend.dummy_data import generate_performance_history, generate_risk_analysis
-from ..models import News
+from ..models import News, Query, MLDatabase
 
 from . import views_predictions, views_admin, views_historical_data, views_account
 
@@ -19,7 +20,7 @@ def index():
 
     notifications = [{"link": '/prediction/1/1/1', "title": 'New Prediction', "mins_ago": 10},
                      {"link": '/performance_history', "title": 'New Performance History', "mins_ago": 15}]
-    majors = ['Informatik', 'Sozialwissenschaften', 'Wirtschaftswissenschaften', 'Rechtswissenschaften', 'all']
+    majors = ['Informatik', 'Sozialwissenschaften', 'all']
     return render_template('main/dashboard.html', news=news, notifications=notifications, page_title=page_title,
                            majors=majors)
 
@@ -28,45 +29,44 @@ def index():
 @main.route('/dashboard-data/<string:major_id>/<string:degree_id>')
 @login_required
 def dashboard_data(major_id, degree_id):
-    # generate dummy performance data
-    performance_df = generate_performance_history(100)
-    # generate dummy risk analysis data
-    dropout_df = generate_risk_analysis(100)
+    db_id = 1
+    db = MLDatabase.query.get_or_404(db_id)
+    query_name = "performance_history"
+    query_string = Query.query.filter_by(name=query_name).first_or_404()
+    performance_df = query_database(db, query_string)
 
     avg_bachelor_semesters = '-'
     avg_master_semesters = '-'
 
-    if major_id == 'all':
-        performance_df = performance_df
-        dropout_df = dropout_df
-    else:
-        performance_df = performance_df[performance_df['Major'] == major_id]
         dropout_df = dropout_df[dropout_df['Major'] == major_id]
 
     if degree_id == 'bachelor':
-        performance_df = performance_df[performance_df['Degree'] == 'Bachelor']
-        dropout_df = dropout_df[dropout_df['Degree'] == 'Bachelor']
+        performance_df = performance_df[performance_df['Abschluss'] == 'Bachelor']
 
     elif degree_id == 'master':
-        performance_df = performance_df[performance_df['Degree'] == 'Master']
-        dropout_df = dropout_df[dropout_df['Degree'] == 'Master']
+    if performance_df.empty:
+        abort(404)
 
     # get total number of students
     total_students = len(performance_df['Matrikel_Nummer'].unique())
-    # get total number of dropouts
-    total_dropouts = len(dropout_df[dropout_df['Dropout'] == 'Yes'])
+    query_string = """
+    Select Count(*) as total_students
+    FROM Einschreibung E
+    Where Exmatrikulationsdatum is ''
+    total_students = query_database(db, query_string)['total_students'][0]
 
     # avg number of semesters for all students
     # group by student id and get sum of ECTS, max number of semesters and degree
-    group_performance = performance_df.groupby('Matrikel_Nummer').agg(
-        {'ECTS': 'sum', 'Num_Semester': 'max', 'Degree': 'max'}).reset_index()
+    group_performance = performance_df.groupby('Pseudonym').agg(
+        {'ECTS': 'sum', 'Fachsemester': 'max', 'Abschluss': 'max'}).reset_index()
 
     if degree_id != 'master':
         # get students that have 180 ECTS and are in bachelor
         bachelor_students = group_performance[
             (group_performance['ECTS'] >= 180) & (group_performance['Degree'] == 'Bachelor')]
+            (group_performance['ECTS'] >= 180) & (group_performance['Abschluss'] == 'Bachelor')]
         # get avg number of semesters for bachelor students
-        avg_bachelor_semesters = round(bachelor_students['Num_Semester'].mean(), 0)
+        avg_bachelor_semesters = round(bachelor_students['Fachsemester'].mean(), 0)
 
         if np.isnan(avg_bachelor_semesters):
             avg_bachelor_semesters = 6
@@ -74,7 +74,7 @@ def dashboard_data(major_id, degree_id):
     if degree_id != 'bachelor':
         # get students that have 120 ECTS and are in master
         master_students = group_performance[
-            (group_performance['ECTS'] >= 120) & (group_performance['Degree'] == 'Master')]
+            (group_performance['ECTS'] >= 120) & (group_performance['Abschluss'] == 'Master')]
         # get avg number of semesters for master students
         avg_master_semesters = round(master_students['Num_Semester'].mean(), 0)
 
@@ -82,24 +82,26 @@ def dashboard_data(major_id, degree_id):
             avg_master_semesters = 4
 
     # avg ects per semester
-    group_performance = performance_df.groupby(['Matrikel_Nummer', 'Semester', 'Year']).agg(
         {'ECTS': 'sum'}).reset_index()
     avg_ects_per_semester = round(group_performance['ECTS'].mean(), 0)
 
     # get data for plots get only unique Matrikel_Nummer
     group_performance = performance_df.groupby('Matrikel_Nummer').agg(
         {'Sex': 'max', 'Nationality': 'max'}).reset_index()
+        {'Geschlecht': 'max', 'Deutsch': 'max'}).reset_index()
 
-    sex_plot = group_performance['Sex'].value_counts().reset_index().sort_values(by='Sex', ascending=False).rename(
-        columns={'index': 'labels', 'Sex': 'data'})
 
-    nat_plot = group_performance['Nationality'].value_counts().reset_index().sort_values(by='Nationality',
-                                                                                         ascending=False).rename(
+    # replace binary labels with actual values
+    group_performance['Deutsch'] = group_performance['Deutsch'].replace(
+        {0: 'Nicht-Deutsch', 1: 'Deutsch'})
+
         columns={'index': 'labels', 'Nationality': 'data'})
+    nat_plot = group_performance['Deutsch'].value_counts().reset_index().sort_values(by='Deutsch',
+                                                                                     ascending=False).rename(
+        columns={'index': 'labels', 'Deutsch': 'data'})
 
     # add all values to response
     response = {'totalStudents': total_students,
-                'totalDropouts': total_dropouts,
                 'avgBachelorSemesters': avg_bachelor_semesters,
                 'avgMasterSemesters': avg_master_semesters,
                 'avgEctsPerSemester': avg_ects_per_semester,
