09652491a5bab47bf6edbd51d456e6fa94eec99c
Jose <jose.cordova@uni-duesseldorf.de>
2023-09-11 13:17:31 +0200

    Refactor to use .ini files and handle errors

--- dev_setup.py
+++ dev_setup.py
@@ -5,39 +5,72 @@ from glob import glob
 from app import db, create_app
 from app.models import MLDatabase, Model, Query, User, queried_by
 
+import configparser
+import logging
+logging.basicConfig(level=logging.INFO)
 
-def setup_queries_and_models(uploads: str, db_id: int) -> None:
+
+def setup_targets(uploads: str, db_id: int) -> None:
+    """
+    Adds all targets to the database
+
+    The targets are the folders in the uploads folder that contain
+    - A .sql file with the query
+    - A .joblib file with the model
+    - A config.ini file with:
+        - A [DEFAULT] section with following keys:
+            - label: the name of the target
+            - description: a description of the target
+            - model: the name of the model
+    """
+
     targets = [f.path for f in os.scandir(uploads) if f.is_dir()]
 
     for target in targets:
+        config_path = os.path.join(target, 'config.ini')
+        if not os.path.exists(config_path):
+            logging.warning(f'config.ini not found in {target}, folder skipped')
+            continue
+
+        config = configparser.ConfigParser()
+        config.read(config_path)
+
+        try:
+            target_name = config['DEFAULT']['label']
+            description = config['DEFAULT'].get('description', '')
+        except KeyError as e:
+            logging.warning(f'config.ini in {target_name} is missing {e} key, folder skipped')
+            continue
+
         model_paths = glob(os.path.join(target, '*.joblib'))
         sql_path = glob(os.path.join(target, '*.sql'))[0]
-        description_path = os.path.join(target, 'description.txt')
 
         # Add query
         with open(sql_path, 'r') as f:
             sql_string = f.read()
-        query = Query(name=os.path.relpath(target, uploads), query_string=sql_string)
-
-        # Add description
-        with open(description_path, 'r') as f:
-            description_text = f.read()
-        query.description = description_text
-
+        query = Query(name=target_name, query_string=sql_string, description=description)
         query.save()
-        print(f'{os.path.relpath(target, uploads)} query added at {query.id}')
+        logging.debug(query)
 
         # Add query and db to relationship
         db.session.execute(queried_by.insert().values(database_id=db_id, query_id=query.id))
         db.session.commit()
 
-        # Add models
+        # Add model
         for model_path in model_paths:
-            model_name = os.path.splitext(os.path.basename(model_path))[0].split('_')[-1]
+            model_name = config['DEFAULT'].get('model', os.path.splitext(os.path.basename(model_path))[0])
             model = Model(name=model_name, filename=os.path.relpath(model_path, uploads), query_id=query.id)
             model.save()
-            print(f'{model_name} model added at {query.id}')
+            logging.debug(model)
 
+        logging.info(f'{target_name} target added')
+
+
+def setup_functional_queries(uploads: str) -> None:
+    """
+    Adds all functional queries to the database
+    Functional queries are .sql files in the uploads folder
+    """
     # Add exams performance query
     # Add module average grade
     for sql in glob(os.path.join(uploads, '*.sql')):
@@ -47,25 +80,31 @@ def setup_queries_and_models(uploads: str, db_id: int) -> None:
         query = Query(name=query_name, query_string=sql_string, is_target=False)
         query.save()
 
-        print(f'{query_name} query added at {query.id}')
+        logging.debug(query)
 
 
 if __name__ == '__main__':
     app = create_app('development')
 
     uploads_path = app.config['UPLOAD_FOLDER']
+    database_path = app.config['SQLALCHEMY_DATABASE_URI']
 
-    # if no database was inputted, use the first one
-    if sys.argv[1] is None:
-        db_path = glob(os.path.join(uploads_path, '*.db'))[0]
-        db_name = os.path.splitext(os.path.basename(db_path))[0]
+    # if no database was inputted, use rapp_dummy.db
+    if len(sys.argv) <= 1:
+        db_path = os.path.join(uploads_path, 'rapp_dummy.db')
+        db_name = 'rapp_dummy'
     else:
         assert os.path.splitext(sys.argv[1])[1] == '.db', 'Database must be a .db file'
+
         db_path = os.path.join(uploads_path, sys.argv[1])
         db_name = os.path.splitext(sys.argv[1])[0]
 
+        if not os.path.exists(db_path):
+            raise FileNotFoundError(f'{db_path} does not exist in {uploads_path}')
+
+    logging.info(f'Using {db_path} as database')
+
     with app.app_context():
-        # Delete all tables
         db.drop_all()
         # Create the database
         db.create_all()
@@ -74,11 +113,12 @@ if __name__ == '__main__':
         user = User(email='admin@admin.com', password='admin', is_admin=True)
         user.save()
 
-        uploads_path = app.config['UPLOAD_FOLDER']
-
         # Add main database
-        
         ml_db = MLDatabase(name=db_name, filename=os.path.relpath(db_path, uploads_path), user_id=user.id)
         ml_db.save()
 
-        setup_queries_and_models(uploads_path, ml_db.id)
+        # setup_queries_and_models(uploads_path, ml_db.id)
+        setup_targets(uploads_path, ml_db.id)
+        setup_functional_queries(uploads_path)
+
+        logging.info('Database setup complete')
